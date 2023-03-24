39e9673636bda9853d012e1256bea3472723eff1
SWADSOURCE <128734547+SWADSOURCE@users.noreply.github.com>
2023-03-24 04:09:07 +0200

    Update decorators.py

--- sbb_b/core/decorators.py
+++ sbb_b/core/decorators.py
@@ -22,7 +22,7 @@ def check_owner(func):
         else:
             HELP_TEXT = (
                 gvarstatus("HELP_TEXT")
-                or "فقط مالك البوت له صلاحيات الدخول هنا\n\@FTTUTY"
+                or "فقط مالك البوت له صلاحيات الدخول هنا\n\@source_av"
             )
             await c_q.answer(
                 HELP_TEXT,
